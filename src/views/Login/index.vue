5449e70a7c9a18b3b8b93f6c07764cabab772e33
ChenJunxing <ChenJunxing@example.com>
2025-07-01 16:50:06 +0800

    登录页-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,8 @@ import { ref } from 'vue'
 // 1.准备表单对象
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree: true
 })
 
 // 2.准备规则对象
@@ -15,6 +16,19 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '密码长度为6-14个字符', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                // 自定义校验逻辑
+                // 勾选就通过 不勾选不通过
+                if (value){
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 }
 
@@ -45,13 +59,13 @@ const rules = {
                     <div class="form">
                         <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
                             <el-form-item prop="account" label="账户">
-                                <el-input v-model="form.account"/>
+                                <el-input v-model="form.account" />
                             </el-form-item>
                             <el-form-item prop="password" label="密码">
-                                <el-input v-model="form.password"/>
+                                <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox size="large" v-model="form.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
