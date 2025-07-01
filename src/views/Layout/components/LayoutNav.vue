470545626e569327eeef801e742bb7c75d45035c
ChenJunxing <ChenJunxing@example.com>
2025-07-01 16:21:42 +0800

    登录页-整体认识和路由配置

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -5,7 +5,7 @@
                 <!-- 多模版渲染 区分登录状态和非登录状态 -->
 
                 <!-- 适配思路: 登录时显示第一块 非登录时显示第二块  是否有token -->
-                <template v-if="true">
+                <template v-if="false">
                     <li><a href="javascript:;"><i class=" iconfont icon-user"></i>周杰伦</a>
                     </li>
                     <li>
