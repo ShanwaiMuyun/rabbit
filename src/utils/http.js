c15f6db40c42cfb53b5dcf276dd7ed082ef521b9
ChenJunxing <ChenJunxing@example.com>
2025-07-02 15:31:52 +0800

    登录页-401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -30,6 +31,14 @@ httpInstance.interceptors.response.use(res => res.data, e => {
         type: 'warning',
         message: e.response.data.message
     })
+    // 401 token 失效处理
+    // 1.清除本地用户数据
+    // 2.跳转登录页面
+    if (e.response.status === 401) {
+        const userStore = useUserStore()
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
