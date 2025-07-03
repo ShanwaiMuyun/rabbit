3aa70fd3f89c4f7ad8752036db1212b67cd6f06f
ChenJunxing <ChenJunxing@example.com>
2025-07-03 14:08:53 +0800

    购物车-本地购物车列表购物车单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -27,6 +27,13 @@ export const useCartStore = defineStore('cart', () => {
         const idx = cartList.value.findIndex((item) => skuId === item.skuId)
         cartList.value.splice(idx, 1)
     }
+    // 单选功能
+    const singleCheck = (skuId, selected) => {
+        // 通过 skuId 找到要修改的那一项，然后把它的 selected 修改为传过来的 selected
+        const item = cartList.value.find((item) => skuId === item.skuId)
+        item.selected = selected
+    }
+
     // 计算属性
     // 1.总的数量 所有项的count之和
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
@@ -37,7 +44,8 @@ export const useCartStore = defineStore('cart', () => {
         allCount,
         allPrice,
         addCart,
-        delCart
+        delCart,
+        singleCheck
     }
 }, {
     persist: true
--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { useCartStore } from '@/stores/cartStore'
 const cartStore = useCartStore()
+
+// 单选回调
+const singleCheck = (i, selected) => {
+    // 除了 selected 外还需要补充一个用来筛选的参数 - skuId
+    cartStore.singleCheck(i.skuId, selected)
+}
 </script>
 
 <template>
@@ -25,7 +31,8 @@ const cartStore = useCartStore()
                         <tr v-for="i in cartStore.cartList" :key="i.id">
                             <td>
                                 <!-- 单选框 -->
-                                <el-checkbox />
+                                <el-checkbox :model-value="i.selected"
+                                    @change="(selected) => singleCheck(i, selected)" />
                             </td>
                             <td>
                                 <div class="goods">
@@ -73,8 +80,8 @@ const cartStore = useCartStore()
             <!-- 操作栏 -->
             <div class="action">
                 <div class="batch">
-                    共 {{ cartStore.allCount }} 件商品，已选择  件，商品合计：
-                    <span class="red">¥  </span>
+                    共 {{ cartStore.allCount }} 件商品，已选择 件，商品合计：
+                    <span class="red">¥ </span>
                 </div>
                 <div class="total">
                     <el-button size="large" type="primary" @click="$router.push('/checkout')">下单结算</el-button>
